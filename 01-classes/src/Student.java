902924eeae101d7cc71c657ac4a8717e86f6fe7e
Matheus <mt121207@gmail.com>
2024-05-02 20:14:51 -0300

    Aula sobre classes.

---
+++ 01-classes/src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    String name;
+    int age;
+    Color color;
+    Sex sex;
+
+    void eating(Food food) {
+        // NOSSO CÓDIGO AQUI
+    }
+
+    void drinking(Drink drink) {
+        // NOSSO CÓDIGO AQUI
+    }
+
+    void running() {
+        // NOSSO CÓDIGO AQUI
+    }
+}
